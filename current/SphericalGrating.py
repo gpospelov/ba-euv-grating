6e8e64930dc2ac862120f9f87643d97eb435c1f7
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-03-07 14:41:30 +0100

    Minor changes in SphericalGrating

--- current/SphericalGrating.py
+++ current/SphericalGrating.py
@@ -5,11 +5,12 @@ three-spheres-shape-like profile grating.
 import bornagain as ba
 from grating_base import GratingBase
 from bornagain import nm, deg
-import utils
+from utils.grating_shape import GratingShape
+from utils.json_utils import load_sample_setup
 
 
 class SphericalGrating(GratingBase):
-    def __init__(self):
+    def __init__(self, setup):
         super().__init__()
         self.m_grating_height = 225*nm
         self.m_grating_bulk = 100.0*nm
@@ -22,13 +23,13 @@ class SphericalGrating(GratingBase):
         return self.m_grating_bulk
 
     def grating(self):
-        grshape = utils.GratingShape(period=self.grating_period(),
+        grshape = GratingShape(period=self.grating_period(),
                                      grating_length=self.grating_length(),
                                      thickness=self.grating_bulk(),
                                      nslices=int(self.m_nslices))
         return grshape.get_composition(self.grating_material())
 
-    def buildSample(self):
+    def buildSample(self, wavelength):
         layout = ba.ParticleLayout()
         layout.addParticle(self.grating(), 1.0,
                            ba.kvector_t(0.0, 0.0, 100.0),
@@ -42,7 +43,7 @@ class SphericalGrating(GratingBase):
         substrate_layer = ba.Layer(self.substrate_material())
 
         roughness = ba.LayerRoughness()
-        roughness.setSigma(25.0*nm)
+        roughness.setSigma(5.0*nm)
         roughness.setHurstParameter(0.5)
         roughness.setLatteralCorrLength(10.0*nm)
 
@@ -52,3 +53,8 @@ class SphericalGrating(GratingBase):
         multi_layer.addLayerWithTopRoughness(substrate_layer, roughness)
 
         return multi_layer
+
+
+def get_sample():
+    sample_config = load_sample_setup("spherical")
+    return SphericalGrating(sample_config).buildSample(1.0)

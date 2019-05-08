81a7c202f3c29a7314af8c538d2c00f609d4da83
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-05-08 14:44:29 +0200

    Preparing fit of spherical grating parameters

--- current/run_simulation.py
+++ current/run_simulation.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
     report = ReportManager()
 
     exp_config = load_experimental_setup("exp2")
-    sample_config = load_sample_setup("sinus")
+    sample_config = load_sample_setup("spherical")
 
     run_single(exp_config, sample_config, report)
     # run_pack(exp_config, sample_config, report)
